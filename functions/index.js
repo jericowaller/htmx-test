96825ef30ad6c9ef318905a4a4339fbae8fc875e
Jerico <jericowaller@gmail.com>
2024-05-06 20:24:56 -0600

    project

--- functions/index.js
+++ functions/index.js
@@ -11,16 +11,35 @@ const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const pug = require('pug');
 
-exports.test = onRequest((request, response) => {
-    let template = pug.compileFile('views/test.pug');
-    let markup = template({
-        name: request.query.name,
-        major: request.query.major,
-        quote: request.query.quote
-    });
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end(markup);
-});
+exports.home = onRequest({ cors: true }, (request, response) => {
+    place = request.query.search;
+    api = process.env.API_KEY
+    let template = pug.compileFile('views/home.pug');
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${api}&units=imperial`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('API request failed');
+            }
+        })
+        .then(data => {
+            let markup = template({
+                name: data.name,
+                temp: Math.round(data.main.temp),
+                feels: Math.round(data.main.feels_like),
+                weather: data.weather[0].main,
+                humidity: data.main.humidity,
+                wind: data.wind.speed
+            });
+            console.log("done");
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(markup);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+})
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
